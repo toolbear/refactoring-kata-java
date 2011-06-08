ef35b902e1da46bcfb7b181886cecd789511f398
Tim Taylor <tim@tool-man.org>
2011-06-08 07:53:00 -0400

    stance 3: break on newlines in test

--- src/test/java/VideoStoreTest.java
+++ src/test/java/VideoStoreTest.java
@@ -19,7 +19,10 @@ public class VideoStoreTest {
     @Test
     public void testSingleNewReleaseStatement() {
         customer.addRental(new Rental(NEW_RELEASE_1, 3));
-        assertEquals("Rental Record for Fred\n\tThe Cell\t9.0\nYou owed 9.0\nYou earned 2 frequent renter points\n",
+        assertEquals("Rental Record for Fred\n" +
+        		"\tThe Cell\t9.0\n" +
+        		"You owed 9.0\n" +
+        		"You earned 2 frequent renter points\n",
                      customer.statement());
     }
 
@@ -27,14 +30,21 @@ public class VideoStoreTest {
     public void testDualNewReleaseStatement() {
         customer.addRental(new Rental(NEW_RELEASE_1, 3));
         customer.addRental(new Rental(NEW_RELEASE_2, 3));
-        assertEquals("Rental Record for Fred\n\tThe Cell\t9.0\n\tThe Tigger Movie\t9.0\nYou owed 18.0\nYou earned 4 frequent renter points\n",
+        assertEquals("Rental Record for Fred\n" +
+        		"\tThe Cell\t9.0\n" +
+        		"\tThe Tigger Movie\t9.0\n" +
+        		"You owed 18.0\n" +
+        		"You earned 4 frequent renter points\n",
                      customer.statement());
     }
 
     @Test
     public void testSingleChildrensStatement() {
         customer.addRental(new Rental(CHILDRENS_MOVIE, 3));
-        assertEquals("Rental Record for Fred\n\tThe Tigger Movie\t1.5\nYou owed 1.5\nYou earned 1 frequent renter points\n",
+        assertEquals("Rental Record for Fred\n" +
+        		"\tThe Tigger Movie\t1.5" +
+        		"\nYou owed 1.5" +
+        		"\nYou earned 1 frequent renter points\n",
                      customer.statement());
     }
 
@@ -44,7 +54,12 @@ public class VideoStoreTest {
         customer.addRental(new Rental(REGULAR_MOVIE_2, 2));
         customer.addRental(new Rental(REGULAR_MOVIE_3, 3));
 
-        assertEquals("Rental Record for Fred\n\tPlan 9 from Outer Space\t2.0\n\t8 1/2\t2.0\n\tEraserhead\t3.5\nYou owed 7.5\nYou earned 3 frequent renter points\n",
+        assertEquals("Rental Record for Fred\n" +
+        		"\tPlan 9 from Outer Space\t2.0\n" +
+        		"\t8 1/2\t2.0\n" +
+        		"\tEraserhead\t3.5\n" +
+        		"You owed 7.5\n" +
+        		"You earned 3 frequent renter points\n",
                      customer.statement());
     }
 
