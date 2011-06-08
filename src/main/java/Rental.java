8afb054691c72f2dc2d311d2c46dd6ff18bffb5f
Tim Taylor <tim@tool-man.org>
2011-06-08 11:18:24 -0400

    stance 14.1: extract movie and days rented to locals

--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -1,36 +1,39 @@
+public class Rental {
+    private final Movie movie;
+    private final int daysRented;
 
-public class Rental
-{
-    public Rental (Movie movie, int daysRented) {
-        this.movie 		= movie;
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
         this.daysRented = daysRented;
     }
 
-    public int getDaysRented () {
+    public int getDaysRented() {
         return daysRented;
     }
 
-    public Movie getMovie () {
+    public Movie getMovie() {
         return movie;
     }
 
     double amount() {
         double thisAmount = 0;
-    
-        switch (getMovie().getPriceCode()) {
+
+        Movie movie = getMovie();
+        int daysRented = getDaysRented();
+        switch (movie.getPriceCode()) {
             case Movie.REGULAR:
                 thisAmount += 2;
-                if (getDaysRented() > 2) {
-                    thisAmount += (getDaysRented() - 2) * 1.5;
+                if (daysRented > 2) {
+                    thisAmount += (daysRented - 2) * 1.5;
                 }
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += getDaysRented() * 3;
+                thisAmount += daysRented * 3;
                 break;
             case Movie.CHILDRENS:
                 thisAmount += 1.5;
-                if (getDaysRented() > 3) {
-                    thisAmount += (getDaysRented() - 3) * 1.5;
+                if (daysRented > 3) {
+                    thisAmount += (daysRented - 3) * 1.5;
                 }
                 break;
         }
@@ -41,7 +44,4 @@ public class Rental
         if (getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1) return 2;
         else return 1;
     }
-
-    private Movie movie;
-    private int daysRented;
 }
