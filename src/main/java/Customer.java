aeb5944db660fcc9ec0f663ead7220ab9d38aacd
Tim Taylor <tim@tool-man.org>
2011-06-08 08:02:19 -0400

    stance 4: attempt amountOwed(), refactor away Vector instead

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,68 +1,63 @@
+import java.util.*;
 
-import java.util.Vector;
-import java.util.Enumeration;
+public class Customer {
+    private final String name;
+    private final List<Rental> rentals = new ArrayList<Rental>();
 
-public class Customer
-{
-    public Customer (String name) {
+    public Customer(String name) {
         this.name = name;
     }
 
-    public void addRental (Rental rental) {
-        rentals.addElement (rental);
+    public void addRental(Rental rental) {
+        rentals.add(rental);
     }
 
-    public String getName () {
+    public String getName() {
         return name;
     }
 
-    public String statement () {
-        double 				totalAmount 			= 0;
-        int					frequentRenterPoints 	= 0;
-        Enumeration 		rentals 				= this.rentals.elements ();
-        String 				result 					= "Rental Record for " + getName () + "\n";
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        String result = "Rental Record for " + getName() + "\n";
 
-        while (rentals.hasMoreElements ()) {
-            double 		thisAmount = 0;
-            Rental 		each = (Rental)rentals.nextElement ();
+        for (Rental each : rentals) {
+            double thisAmount = 0;
 
             // determines the amount for each line
-            switch (each.getMovie ().getPriceCode ()) {
+            switch (each.getMovie().getPriceCode()) {
                 case Movie.REGULAR:
                     thisAmount += 2;
-                    if (each.getDaysRented () > 2)
-                        thisAmount += (each.getDaysRented () - 2) * 1.5;
+                    if (each.getDaysRented() > 2) {
+                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    }
                     break;
                 case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented () * 3;
+                    thisAmount += each.getDaysRented() * 3;
                     break;
                 case Movie.CHILDRENS:
                     thisAmount += 1.5;
-                    if (each.getDaysRented () > 3)
-                        thisAmount += (each.getDaysRented () - 3) * 1.5;
+                    if (each.getDaysRented() > 3) {
+                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    }
                     break;
             }
 
             frequentRenterPoints++;
 
-            if (each.getMovie ().getPriceCode () == Movie.NEW_RELEASE
-                    && each.getDaysRented () > 1)
+            if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE && each.getDaysRented() > 1) {
                 frequentRenterPoints++;
+            }
 
-            result += "\t" + each.getMovie ().getTitle () + "\t"
-                                + String.valueOf (thisAmount) + "\n";
+            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
 
         }
 
-        result += "You owed " + String.valueOf (totalAmount) + "\n";
-        result += "You earned " + String.valueOf (frequentRenterPoints) + " frequent renter points\n";
-
+        result += "You owed " + String.valueOf(totalAmount) + "\n";
+        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points\n";
 
         return result;
     }
 
-
-    private String name;
-    private Vector rentals = new Vector ();
 }
--- src/test/java/VideoStoreTest.java
+++ src/test/java/VideoStoreTest.java
@@ -1,4 +1,5 @@
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
 
 import org.junit.*;
 
@@ -17,35 +18,32 @@ public class VideoStoreTest {
     }
 
     @Test
+    @Ignore
+    public void singleNewReleaseAmountOwed() {
+        customer.addRental(new Rental(NEW_RELEASE_1, 3));
+        assertThat(customer.amountOwed(), is(9.0));
+    }
+
+    @Test
     public void testSingleNewReleaseStatement() {
         customer.addRental(new Rental(NEW_RELEASE_1, 3));
-        assertEquals("Rental Record for Fred\n" +
-        		"\tThe Cell\t9.0\n" +
-        		"You owed 9.0\n" +
-        		"You earned 2 frequent renter points\n",
-                     customer.statement());
+        assertEquals("Rental Record for Fred\n" + "\tThe Cell\t9.0\n" + "You owed 9.0\n"
+                     + "You earned 2 frequent renter points\n", customer.statement());
     }
 
     @Test
     public void testDualNewReleaseStatement() {
         customer.addRental(new Rental(NEW_RELEASE_1, 3));
         customer.addRental(new Rental(NEW_RELEASE_2, 3));
-        assertEquals("Rental Record for Fred\n" +
-        		"\tThe Cell\t9.0\n" +
-        		"\tThe Tigger Movie\t9.0\n" +
-        		"You owed 18.0\n" +
-        		"You earned 4 frequent renter points\n",
-                     customer.statement());
+        assertEquals("Rental Record for Fred\n" + "\tThe Cell\t9.0\n" + "\tThe Tigger Movie\t9.0\n" + "You owed 18.0\n"
+                     + "You earned 4 frequent renter points\n", customer.statement());
     }
 
     @Test
     public void testSingleChildrensStatement() {
         customer.addRental(new Rental(CHILDRENS_MOVIE, 3));
-        assertEquals("Rental Record for Fred\n" +
-        		"\tThe Tigger Movie\t1.5" +
-        		"\nYou owed 1.5" +
-        		"\nYou earned 1 frequent renter points\n",
-                     customer.statement());
+        assertEquals("Rental Record for Fred\n" + "\tThe Tigger Movie\t1.5" + "\nYou owed 1.5"
+                     + "\nYou earned 1 frequent renter points\n", customer.statement());
     }
 
     @Test
@@ -54,12 +52,8 @@ public class VideoStoreTest {
         customer.addRental(new Rental(REGULAR_MOVIE_2, 2));
         customer.addRental(new Rental(REGULAR_MOVIE_3, 3));
 
-        assertEquals("Rental Record for Fred\n" +
-        		"\tPlan 9 from Outer Space\t2.0\n" +
-        		"\t8 1/2\t2.0\n" +
-        		"\tEraserhead\t3.5\n" +
-        		"You owed 7.5\n" +
-        		"You earned 3 frequent renter points\n",
+        assertEquals("Rental Record for Fred\n" + "\tPlan 9 from Outer Space\t2.0\n" + "\t8 1/2\t2.0\n"
+                             + "\tEraserhead\t3.5\n" + "You owed 7.5\n" + "You earned 3 frequent renter points\n",
                      customer.statement());
     }
 
