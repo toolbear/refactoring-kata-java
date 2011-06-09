47ed26e1829cffecf1e1c2faf56ef9db79ee08a2
Tim Taylor <tim@tool-man.org>
2011-06-09 13:11:39 -0400

    stance 2: eliminate test setup duplication

--- src/test/java/VideoStoreTest.java
+++ src/test/java/VideoStoreTest.java
@@ -1,10 +1,17 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
 
-public class VideoStoreTest
-{
+public class VideoStoreTest {
+    private static final Movie NEW_RELEASE_1 = new Movie ("The Cell", Movie.NEW_RELEASE);
+    private static final Movie NEW_RELEASE_2 = new Movie ("The Tigger Movie", Movie.NEW_RELEASE);
+    private static final Movie REGULAR_MOVIE_1 = new Movie ("Plan 9 from Outer Space", Movie.REGULAR);
+    private static final Movie REGULAR_MOVIE_2 = new Movie ("8 1/2", Movie.REGULAR);
+    private static final Movie REGULAR_MOVIE_3 = new Movie ("Eraserhead", Movie.REGULAR);
+    private static final Movie CHILDRENS_MOVIE = new Movie ("The Tigger Movie", Movie.CHILDRENS);
+
+    private Customer customer;
 
     @Before
     public void setUp ()  {
@@ -13,31 +20,29 @@ public class VideoStoreTest
 
     @Test
     public void testSingleNewReleaseStatement () {
-        customer.addRental (new Rental (new Movie ("The Cell", Movie.NEW_RELEASE), 3));
+        customer.addRental (new Rental (NEW_RELEASE_1, 3));
         assertEquals ("Rental Record for Fred\n\tThe Cell\t9.0\nYou owed 9.0\nYou earned 2 frequent renter points\n", customer.statement ());
     }
 
     @Test
     public void testDualNewReleaseStatement () {
-        customer.addRental (new Rental (new Movie ("The Cell", Movie.NEW_RELEASE), 3));
-        customer.addRental (new Rental (new Movie ("The Tigger Movie", Movie.NEW_RELEASE), 3));
+        customer.addRental (new Rental (NEW_RELEASE_1, 3));
+        customer.addRental (new Rental (NEW_RELEASE_2, 3));
         assertEquals ("Rental Record for Fred\n\tThe Cell\t9.0\n\tThe Tigger Movie\t9.0\nYou owed 18.0\nYou earned 4 frequent renter points\n", customer.statement ());
     }
 
     @Test
     public void testSingleChildrensStatement () {
-        customer.addRental (new Rental (new Movie ("The Tigger Movie", Movie.CHILDRENS), 3));
+        customer.addRental (new Rental (CHILDRENS_MOVIE, 3));
         assertEquals ("Rental Record for Fred\n\tThe Tigger Movie\t1.5\nYou owed 1.5\nYou earned 1 frequent renter points\n", customer.statement ());
     }
 
     @Test
     public void testMultipleRegularStatement () {
-        customer.addRental (new Rental (new Movie ("Plan 9 from Outer Space", Movie.REGULAR), 1));
-        customer.addRental (new Rental (new Movie ("8 1/2", Movie.REGULAR), 2));
-        customer.addRental (new Rental (new Movie ("Eraserhead", Movie.REGULAR), 3));
+        customer.addRental (new Rental (REGULAR_MOVIE_1, 1));
+        customer.addRental (new Rental (REGULAR_MOVIE_2, 2));
+        customer.addRental (new Rental (REGULAR_MOVIE_3, 3));
 
         assertEquals ("Rental Record for Fred\n\tPlan 9 from Outer Space\t2.0\n\t8 1/2\t2.0\n\tEraserhead\t3.5\nYou owed 7.5\nYou earned 3 frequent renter points\n", customer.statement ());
     }
-
-    private Customer customer;
 }
