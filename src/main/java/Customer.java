e06da2b5c50b88c9aeabf8c52f9c6f05d3cdf005
Tim Taylor <tim@tool-man.org>
2011-06-11 02:30:27 -0400

    stance 7.6: format Customer.java

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -5,15 +5,15 @@ public class Customer {
     private String name;
     private List<Rental> rentals = new ArrayList<Rental>();
 
-    public Customer (String name) {
+    public Customer(String name) {
         this.name = name;
     }
 
-    public void addRental (Rental rental) {
-        rentals.add (rental);
+    public void addRental(Rental rental) {
+        rentals.add(rental);
     }
 
-    public String getName () {
+    public String getName() {
         return name;
     }
 
@@ -33,25 +33,21 @@ public class Customer {
         return points;
     }
 
-    public String statement () {
-        String              result                  = "Rental Record for " + getName () + "\n";
+    public String statement() {
+        String result = "Rental Record for " + getName() + "\n";
 
         for (Rental rental : rentals) {
-
-            result += "\t" + rental.getMovie ().getTitle () + "\t"
-                                + String.valueOf (rentalAmount(rental)) + "\n";
+            result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rentalAmount(rental)) + "\n";
         }
 
-        result += "You owed " + String.valueOf (amountOwed()) + "\n";
-        result += "You earned " + String.valueOf (earnedFrequentRenterPoints()) + " frequent renter points\n";
-
+        result += "You owed " + String.valueOf(amountOwed()) + "\n";
+        result += "You earned " + String.valueOf(earnedFrequentRenterPoints()) + " frequent renter points\n";
 
         return result;
     }
 
     private int frequentRenterPoints(Rental rental) {
-        if (rental.getMovie ().getPriceCode () == Movie.NEW_RELEASE
-                && rental.getDaysRented () > 1) {
+        if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1) {
             return 2;
         } else {
             return 1;
@@ -59,22 +55,22 @@ public class Customer {
     }
 
     private double rentalAmount(Rental rental) {
-        double      thisAmount = 0;
+        double thisAmount = 0;
 
-        switch (rental.getMovie ().getPriceCode ()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (rental.getDaysRented () > 2)
-                    thisAmount += (rental.getDaysRented () - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += rental.getDaysRented () * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (rental.getDaysRented () > 3)
-                    thisAmount += (rental.getDaysRented () - 3) * 1.5;
-                break;
+        switch (rental.getMovie().getPriceCode()) {
+        case Movie.REGULAR:
+            thisAmount += 2;
+            if (rental.getDaysRented() > 2)
+                thisAmount += (rental.getDaysRented() - 2) * 1.5;
+            break;
+        case Movie.NEW_RELEASE:
+            thisAmount += rental.getDaysRented() * 3;
+            break;
+        case Movie.CHILDRENS:
+            thisAmount += 1.5;
+            if (rental.getDaysRented() > 3)
+                thisAmount += (rental.getDaysRented() - 3) * 1.5;
+            break;
         }
         return thisAmount;
     }
