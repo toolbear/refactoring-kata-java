ec2b7299436857fb4141703300f92a4f914c9aa0
Tim Taylor <tim@tool-man.org>
2011-06-11 19:59:35 -0400

    stance 10.3: move frequent renter points behavior to Movie

--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -20,10 +20,6 @@ public class Rental {
     }
 
     int frequentRenterPoints() {
-        if (getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1) {
-            return 2;
-        } else {
-            return 1;
-        }
+        return getMovie().frequentRenterPoints(getDaysRented());
     }
 }
